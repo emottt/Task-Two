d270ade5f09e1048f546fe02cbafdd48d8fd662d
emottt <31847517+emottt@users.noreply.github.com>
2022-02-21 11:28:26 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace netSalary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var salary = int.Parse(Console.ReadLine());
+
+            var result = new Salary(salary);
+
+            Console.WriteLine(result.NetSalary());
+
+        }
+
+
+
+    }
+
+}
+    
+
---
+++ Salary.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace netSalary
+{
+    class Salary
+    {
+
+
+        private readonly decimal salary;
+        public decimal GetSalary() => salary;
+        public Salary(decimal salary)
+        {
+            this.salary = salary;
+        }
+
+        public const int maxAmounts = 3000;
+        public const int noTaxation = 1000;
+
+        // calculation all net salary
+        public decimal NetSalary() 
+        {
+            decimal result = 0M;
+            if(GetSalary() > 1000)
+            {
+                result = GetSalary() - Taxes();
+            }
+            return result;
+        }
+
+        // calculation and return of all due contributions
+        public  decimal Taxes()
+        {
+
+            var incomeResult = IncomeTax();
+            var socialResult = SocialContributions();
+
+            var allTaxes = incomeResult + socialResult;
+
+            return allTaxes;
+        }
+
+        // calculation and return of due social contributions
+        public  decimal SocialContributions()
+        {
+            var result = GetSalary();
+            if (result > maxAmounts)
+            {
+                result = maxAmounts;
+            }
+
+            result -= noTaxation;
+
+            decimal socialTax = result * 0.15M;
+            return socialTax;
+        }
+
+        // calculation and refund of tax due
+        public decimal IncomeTax()
+        {
+            var result = GetSalary();
+
+            result -= noTaxation;
+
+            decimal incomeTax = result * 0.1M;
+
+            return incomeTax;
+        }
+
+        // convert GetSalary in string
+        public override string ToString()
+        {
+            
+            return $"{this.GetSalary()}";
+        }
+    }
+
+    
+}
+
+
+
+
+
+
